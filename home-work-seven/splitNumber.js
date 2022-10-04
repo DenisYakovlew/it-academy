7d5e859208f8f67c6b3d0c2418b14bcdd7610812
Denis Yakovlev <d.yakovlev@leningrad.io>
2022-10-04 18:44:05 +0400

    Fix confitions of type in splitNumber.js, add package.json

--- home-work-seven/splitNumber.js
+++ home-work-seven/splitNumber.js
@@ -1,34 +1,34 @@
 function splitNumberToInteger(numberToSplit, countPart) {
-    if (numberToSplit >= countPart && countPart > 0)
+    if (numberToSplit >= countPart && countPart > 0 && typeof numberToSplit === 'number' && typeof countPart === 'number')
     {  
         let necessaryNumbers = new Array(countPart - 1);
         let sumNecessaryNumber = 0;
         for (let i = 0; i < countPart - 1; i++) {
-            necessaryNumbers[i] = Math.ceil(Math.random() * (numberToSplit / countPart))
-            sumNecessaryNumber += necessaryNumbers[i]
+            necessaryNumbers[i] = Math.ceil(Math.random() * (numberToSplit / countPart));
+            sumNecessaryNumber += Number(necessaryNumbers[i]);
         }
-        necessaryNumbers.push(numberToSplit - sumNecessaryNumber)
+        necessaryNumbers.push(numberToSplit - sumNecessaryNumber);
         console.log(`Число ${numberToSplit} разбито на ${countPart}, получившееся числа ${necessaryNumbers}`);
     } else {
-        console.log(`Число частей для разбиения числа не должно превышать исходное число, и должно быть больше нуля`)
+        console.log(`Число частей для разбиения числа не может быть > исходного числа, и должно быть больше нуля. Передаваемые параметры должны быть типом 'number'`);
     }
 }
 
 function splitNumberToFloat(numberToSplit, countPart) {
-    if (numberToSplit >= countPart && countPart > 0)
+    if (numberToSplit >= countPart && countPart > 0 && typeof numberToSplit === 'number' && typeof countPart === 'number')
     {  
         let necessaryNumbers = new Array(countPart - 1);
         let sumNecessaryNumber = 0;
         for (let i = 0; i < countPart - 1; i++) {
             necessaryNumbers[i] = (Math.random() * (numberToSplit / countPart)).toFixed(2);
-            sumNecessaryNumber += Number(necessaryNumbers[i])
+            sumNecessaryNumber += Number(necessaryNumbers[i]);
         }
         necessaryNumbers.push((numberToSplit - sumNecessaryNumber).toFixed(2))
         console.log(`Число ${numberToSplit} разбито на ${countPart}, получившееся числа ${necessaryNumbers}`);
     } else {
-        console.log(`Число частей для разбиения числа не должно превышать исходное число, и должно быть больше нуля`)
+        console.log(`Число частей для разбиения числа не может быть > исходного числа, и должно быть больше нуля. Передаваемые параметры должны быть типом 'number'`);
     }
 }
 
-splitNumberToInteger(23, 5)
-splitNumberToFloat(15, 3)
+splitNumberToInteger(67, 5);
+splitNumberToFloat(34, 14);
