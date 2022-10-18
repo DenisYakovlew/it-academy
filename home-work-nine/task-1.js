f03b18f0e09c7e4e8cbfae803f146323dd2cdcfd
Denis Yakovlev <d.yakovlev@leningrad.io>
2022-10-18 17:00:30 +0400

    fix something

--- home-work-nine/task-1.js
+++ home-work-nine/task-1.js
@@ -1,9 +1,4 @@
-// Сделайте 3 промиса, в каждом из которых расположена функция setTimeout со случайной задержкой от 1 до 5 секунд. 
-// Пусть первый промис возвращает число 1, второе - число 2, третий - число 3.
-// С помощью Promise.race дождитесь загрузки первого сработавшего промиса и выведите результат его работы на экран.
 let getRandomNumber = (min, max) => { return Math.ceil(Math.random() * (max - min + 1)) * 1000 }; 
-
-
 const onePromise = new Promise(function(resolve, reject) {
     setTimeout(() => resolve(1), getRandomNumber(1, 5));
   });
