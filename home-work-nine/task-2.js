c444080fce686c5fb1836c3103f71c87654a338a
Denis Yakovlev <d.yakovlev@leningrad.io>
2022-10-18 19:22:35 +0400

    fix task2

--- home-work-nine/task-2.js
+++ home-work-nine/task-2.js
@@ -2,18 +2,18 @@
 // который с задержкой в 3 секунды выведет случайное число от 1 до 5. 
 // Создайте async функцию, которая с помощью await будет дожидаться результата getNum, 
 // затем возводить его в квадрат и выводить на экран.
+let getRandomNumber = (min, max) => { return Math.ceil(Math.random() * (max - min) + min) }
 
 function getNum () {
-    const randomNumber = Math.ceil(Math.random() * (5 - 1) + 1);
     return new Promise((resolve, reject) => {
-        setTimeout(() => resolve(randomNumber), 3000);
-    })
+        setTimeout(() => resolve(getRandomNumber(1, 5)), 3000);
+    });
 }
 
 async function result() {
-    return `Число из getNum в квадрате равно ${Math.pow(await getNum(), 2)}`
+    return `Число возведенное в квадрат из getNum ${Math.pow(await getNum(), 2)}`
 }
 
-result().then(value => {
-    console.log(value);
-});
+result().then(data => {
+    console.log(data)
+})
